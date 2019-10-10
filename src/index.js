262de83989cdd26fde7aca07bf2a8eaf437fa2cc
David Huang <bugman195@gmail.com>
2019-10-10 22:38:29 +0800

    Add Provider HOC

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 import App from 'components/App';
 import GlobalStyles from 'styles/globals';
+import configStore from 'store';
 
 const renderApp = () => {
+    const store = configStore({});
+
+    if (process.env.NODE_ENV !== 'production') {
+        console.log('Init state ->');
+        console.log(store.getState());
+    }
+
     ReactDOM.render(
-        <div>
-            <GlobalStyles />
-            <App />
-        </div>,
+        <Provider store={store}>
+            <div>
+                <GlobalStyles />
+                <App />
+            </div>
+        </Provider>,
         document.getElementById('root')
     );
 };
